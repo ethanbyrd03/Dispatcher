e24ab8cd31864e92d71f49fa58b9041d33631993
root <ethbyr@ad.unc.edu>
2023-11-20 18:22:22 +0000

    dispatcher

--- sim.c
+++ sim.c
@@ -30,28 +30,15 @@
 //    None
            
 void dispatcher(unsigned int cycles_per_task) {
-    struct task_struct* head = NULL;
     schedule();
-    while (head != NULL) {
-        struct task_struct* highestPriorityTask = head;
-        if (highestPriorityTask != NULL) {
-            highestPriorityTask->remaining_cycles -= cycles_per_task;
-            if (highestPriorityTask->remaining_cycles <= 0) {
-                struct task_struct* result = remove_task(highestPriorityTask->pid);
-                if (result != NULL) {
-                    printf("Task with PID %u completed\n", highestPriorityTask->pid);
-                    free(highestPriorityTask);
-                } else {
-                    printf("Error removing completed task (PID: %u)\n", highestPriorityTask->pid);
-                }
-            } else {
-                unsigned int result = append_task(highestPriorityTask->pid, highestPriorityTask->priority, highestPriorityTask->remaining_cycles);
-                if (result == 0) {
-                    printf("Task with PID %u appended back to the queue\n", highestPriorityTask->pid);
-                } else {
-                    printf("Error appending task back to the queue (PID: %u)\n", highestPriorityTask->pid);
-                }
-            }
+    int headIndex = 0;
+    while (size() > 0) {
+        struct task_struct* currentTask = remove_task(get_task(headIndex)->pid);
+        if (currentTask == NULL) {
+        return;}
+        currentTask->remaining_cycles = currentTask->remaining_cycles - cycles_per_task;
+        if (currentTask->remaining_cycles > 0) {
+           append_task(currentTask->pid, currentTask->priority, currentTask->remaining_cycles);
         }
         schedule();
     }
@@ -75,19 +62,14 @@ void dispatcher(unsigned int cycles_per_task) {
 
 void run(task_struct* task, unsigned int num_cycles) {
     if (task == NULL) {
-        printf("Invalid task pointer\n");
         return;
     }
     if (task->remaining_cycles <= 0) {
-        printf("Task with PID %u is already completed\n", task->pid);
         return;
     }
     if (num_cycles >= (unsigned) task->remaining_cycles) {
-        printf("Task with PID %u run for %u cycle(s).\n", task->pid, task->remaining_cycles);
-        printf("Task with PID %u completed.\n", task->pid);
         task->remaining_cycles = 0;
     } else {
-        printf("Task with PID %u run for %u cycle(s).\n", task->pid, num_cycles);
         task->remaining_cycles -= num_cycles;
     } 
 } // end run() function
